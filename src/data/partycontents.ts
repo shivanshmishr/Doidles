70d82d0db5a6bb272c5a83efdcf16ab7091ecdb5
dhiraj-comxr <159617474+dhirajcomxr@users.noreply.github.com>
2025-09-26 11:07:12 +0530

    changes in the code file

--- src/data/partycontents.ts
+++ src/data/partycontents.ts
@@ -2,65 +2,65 @@
 export const galleryItems = [
   {
     id: 1,
-    src: "/main/games/Arcade.jpg",
+    src: "/main/games/Arcadegames.jpg",
     alt: "Card1",
     aspectRatio: "aspect-[3/4]",
-    video:"/main/videos/1.mp4"
+    video:"/main/videos/Arcade.mp4"
   },
   {
     id: 2,
     src: "/main/games/Bowling.jpg",
     alt: "Card2",
     aspectRatio: "aspect-[4/3]",
-    video:"/main/videos/2.mp4"
+    video:"/main/videos/bowlling.mp4"
   },
   {
     id: 3,
     src: "/main/games/Cricket.jpg",
     alt: "Card3",
     aspectRatio: "aspect-square",
-    video:"/main/videos/3.mp4"
+    video:"/main/videos/cricket.mp4"
   },
   {
     id: 4,
     src: "/main/games/Dashingcar.jpg",
     alt: "Card4",
     aspectRatio: "aspect-square",
-    video:"/video/video1.MOV"
+    video:"main/videos/Dashingcar.mp4"
   },
   {
     id: 5,
     src: "/main/games/card1.jpg",
     alt: "Card5",
     aspectRatio: "aspect-[3/4]",
-    video:"/main/videos/4.mp4"
+    video:"/main/videos/vr.mp4"
   },
   {
     id: 6,
     src: "/main/games/Trampoline.jpg",
     alt: "Card6",
     aspectRatio: "aspect-[4/3]",
-    video:"/main/videos/5.mp4"
+    video:"/main/videos/trempoline.mp4"
   },
   {
     id: 7,
     src: "/main/games/card7.jpg",
     alt: "Card7",
     aspectRatio: "aspect-[4/3]",
-    video:"/main/videos/1.mp4"
+    video:"/main/videos/zenith.mp4"
   },
   {
     id: 8,
     src: "/main/games/card8.jpg",
     alt: "Card8",
     aspectRatio: "aspect-[3/4]",
-    video:"/main/videos/2.mp4"
+    video:"/main/videos/Birthday.mp4"
   },
   {
     id: 9,
     src: "/main/games/Softplay.jpg",
     alt: "Card9",
     aspectRatio: "aspect-square",
-    video:"/main/videos/3.mp4"
+    video:"/main/videos/Softplay.mp4"
   },
 ];
