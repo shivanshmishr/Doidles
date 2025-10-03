cc64b747495d1a83237695cdc89402596438c3eb
dhiraj-comxr <159617474+dhirajcomxr@users.noreply.github.com>
2025-10-03 16:58:53 +0530

    push fo location

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -4,14 +4,15 @@ export const locations = [
     name: "Kullu, HP",
     styles: "top-[15%] left-[33%]",
     iframeSrc:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13045.59017555453!2d75.71656923631879!3d29.1520447387963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391233007aabb4c7%3A0xed9221489df01754!2sDoodles!5e1!3m2!1sen!2sin!4v1752614311429!5m2!1sen!2sin",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.545002151077!2d77.10863857532517!3d31.961060525237787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3904f7ae10324277%3A0xe1ea27bf0c6b7316!2sKullu%20Central%20Mall!5e1!3m2!1sen!2sin!4v1753865656482!5m2!1sen!2sin"
   },
   {
     id: 2,
     name: "Hisar, Haryana",
     styles: "top-[22%] left-[30%]",
     iframeSrc:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.545002151077!2d77.10863857532517!3d31.961060525237787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3904f7ae10324277%3A0xe1ea27bf0c6b7316!2sKullu%20Central%20Mall!5e1!3m2!1sen!2sin!4v1753865656482!5m2!1sen!2sin"
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13045.59017555453!2d75.71656923631879!3d29.1520447387963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391233007aabb4c7%3A0xed9221489df01754!2sDoodles!5e1!3m2!1sen!2sin!4v1752614311429!5m2!1sen!2sin",
+
   },
 
   {
@@ -29,11 +30,11 @@ export const locations = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3748.1891504333134!2d77.05798747479496!3d11.002929254975497!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba857b203ac51f5%3A0x84b28d7083bac205!2sSRK%20Miraj%20cinemas!5e1!3m2!1sen!2sin!4v1753865937592!5m2!1sen!2sin",
   },
   {
-  id: 5,
-  name: "Sirsa, Haryana",
-  styles: "top-[24%] left-[28%]",
-  iframeSrc:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2490.6540125885394!2d75.12988857379682!3d29.529566742784887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39114fbdddaf73cb%3A0xc789c851b2ac61f8!2sOHM%20Cine%20Garden%20%26%20Shopping%20Complex!5e1!3m2!1sen!2sin!4v1756811291339!5m2!1sen!2sin",
-}
+    id: 5,
+    name: "Sirsa, Haryana",
+    styles: "top-[24%] left-[28%]",
+    iframeSrc:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2490.6540125885394!2d75.12988857379682!3d29.529566742784887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39114fbdddaf73cb%3A0xc789c851b2ac61f8!2sOHM%20Cine%20Garden%20%26%20Shopping%20Complex!5e1!3m2!1sen!2sin!4v1756811291339!5m2!1sen!2sin",
+  }
 
 ];
